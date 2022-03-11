f6eb2e208384ba3476492ae4d4f51d73c0f1cfcf
Joe Koop <joe@joekoop.com>
2022-03-10 21:56:36 -0600

    added basic auth to adminer

--- public/adminer/index.php
+++ public/adminer/index.php
@@ -1,4 +1,29 @@
 <?php
+
+// read ADMINER_PASSWORD from .env
+$password = file_get_contents(__DIR__ . '/../../.env');
+$password = explode("\n", $password);
+$password = array_map('trim', $password);
+$password = array_filter($password, function ($value) {
+    return strpos($value, 'ADMINER_PASSWORD') === 0;
+});
+$password = array_pop($password);
+
+if ($password === null) {
+    header('HTTP/1.1 500 Internal Server Error');
+    die('ADMINER_PASSWORD not found in .env');
+}
+
+$password = explode('=', $password);
+$password = array_pop($password);
+
+// basic authentication
+if ($_SERVER['PHP_AUTH_USER'] != 'adminer' || $_SERVER['PHP_AUTH_PW'] != $password) {
+    header('WWW-Authenticate: Basic realm="Adminer"');
+    header('HTTP/1.0 401 Unauthorized');
+    exit;
+}
+
 function adminer_object() {
     // Required to run any plugin.
     include_once(__DIR__ . "/plugins/plugin.php");
