326fc402e5888ac519678f94205f00b4fa93cd97
Tim Koop <github@timkoop.com>
2022-03-31 20:42:17 -0500

    Added hook feedback to the hook layout.

--- resources/views/layouts/hook.blade.php
+++ resources/views/layouts/hook.blade.php
@@ -5,8 +5,18 @@
 
 	<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 
-	<div class="py-12">
+	<div class="my-6">
 		<div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+
+			@if (session()->has('errors') || session()->has('status'))
+				<div>
+					<!-- Session Status -->
+					<x-auth-session-status class="mb-4" :status="session('status')" />
+
+					<!-- Validation Errors -->
+					<x-auth-validation-errors class="mb-4" :status="session('errors')" />
+				</div>
+			@endif
 
 			<div class="flex">
 				<div class="flex flex-col pr-2">
@@ -23,7 +33,8 @@
 					@if ($hook->deployments()->count() > 0)
 						<div>Deployments</div>
 
-						<x-button-link href="/deployment/{{ $hook->deployments()->latest()->first()->id }}" class="w-full mb-2">Latest
+						<x-button-link href="/deployment/{{ $hook->deployments()->latest()->first()->id }}" class="w-full mb-2">
+							Latest
 						</x-button-link>
 
 						<x-button-link href="/hook/{{ $hook->id }}/deployments" class="w-full mb-2">All</x-button-link>
